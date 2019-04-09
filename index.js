890e5a110636128c18b19275f88a6760cd09bf4e
adnangog <adnangog@gmail.com>
2019-04-09 18:54:22 +0300

    ...test

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ import configureStore from './utils/store';
 
 const store = configureStore()
 
+
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
