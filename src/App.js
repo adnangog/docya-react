9560364a0d1f41fe3ad28994726c5fe5ffd34eb0
adnangog <adnangog@gmail.com>
2019-04-15 01:57:26 +0300

    ...

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { Layout } from 'antd';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { connect, Provider } from 'react-redux';
+import { connect } from 'react-redux';
+import decode from 'jwt-decode';
 
 import './App.css';
 import 'antd/dist/antd.css';
@@ -14,6 +15,28 @@ import Login from './containers/login/login';
 import Users from './containers/users/';
 import UserDetail from './containers/users/detail';
 
+const checkAuth = () => {
+
+  const token = localStorage.getItem('token');
+  if (!token) {
+    return false;
+  }
+
+  try {
+    const decoded = decode(token);
+debugger;
+    if (decoded.exp < new Date().getTime() / 1000) {
+      return false;
+    }
+
+  } catch (e) {
+    return false;
+  }
+
+
+  return true;
+}
+
 class App extends Component {
 
   render() {
@@ -24,14 +47,15 @@ class App extends Component {
           <Header />
           <Layout.Content style={{ marginTop: 55 }}>
           
-            { this.props.isLogged  &&<Switch>
+            { checkAuth  &&<Switch>
               <Route exact path="/" component={ Master } />
+              <Route path="/users/add" component={UserDetail} />
               <Route path="/users/edit/:userId" component={UserDetail} />
               <Route path="/users/:userId" component={UserDetail} />
               <Route path="/users" component={Users} />
             </Switch>}
             {
-              !this.props.isLogged  &&<Switch>
+              !checkAuth  &&<Switch>
               <Route component={Login} />
             </Switch>}
             }
